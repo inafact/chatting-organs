bf3284d3d5981095e71fa1e97f9696bccc042cea
Takanobu INAFUKU <takanobu.inafact@gmail.com>
2026-02-09 18:24:26 +0900

    dev snapshot 20260209-1

--- chatting-organs-pipeline/main.py
+++ chatting-organs-pipeline/main.py
@@ -2,6 +2,7 @@ import os
 from pathlib import Path
 from datetime import datetime
 import asyncio
+from random import sample, choice
 
 from dotenv import load_dotenv
 
@@ -25,23 +26,80 @@ class PipelineManager:
     self.pipeline_running = False
     self.player_address = player_address
     self.player_port = player_port
+    self.voices_gemini = {
+      "Zephyr": "Bright"
+      , "Puck": "Upbeat"
+      , "Charon": "情報が豊富"
+      , "Kore": "Firm"
+      , "Fenrir": "Excitable"
+      , "Leda": "Youthful"
+      , "Orus": "Firm"
+      , "Aoede": "Breezy"
+      , "Callirrhoe": "のんびり屋"
+      , "Autonoe": "Bright"
+      , "Enceladus": "Breathy"
+      , "Iapetus": "Clear"
+      , "Umbriel": "Easy-going"
+      , "Algieba": "Smooth"
+      , "Despina": "Smooth"
+      , "Erinome": "クリア"
+      , "Algenib": "Gravelly"
+      , "Rasalgethi": "情報が豊富"
+      , "Laomedeia": "アップビート"
+      , "Achernar": "Soft"
+      , "Alnilam": "Firm"
+      , "Schedar": "Even"
+      , "Gacrux": "成人向け"
+      , "Pulcherrima": "転送"
+      , "Achird": "フレンドリー"
+      , "Zubenelgenubi": "カジュアル"
+      , "Vindemiatrix": "Gentle"
+      , "Sadachbia": "Lively"
+      , "Sadaltager": "知識が豊富"
+      , "Sulafat": "Warm"
+    }
 
   def run_pipeline(self, client_address, address, *args):
+    paths: list
+
+    if len(args) == 1:
+      voices = [
+        args[0],
+        choice(
+          list(filter(lambda k: k != args[0], self.voices_gemini.keys()))
+        )
+      ]
+      paths = self._run_pipeline()
+    elif len(args) > 1:
+      paths = self._run_pipeline(voices=args)
+    else:
+      paths = self._run_pipeline()
+
+    SimpleUDPClient(self.player_address, self.player_port).send_message(
+      "/load_files", paths
+    )
+    SimpleUDPClient(client_address[0], 12001).send_message("/reply", 1)
+
+  def _run_pipeline(self, voices = ["Kore",  "Enceladus"]) -> list:
     if self.pipeline_running:
       print("pipline is running..")
-      return
+      return []
+    if len(voices) < 2:
+      print("error")
+      return []
 
+    print(f"use voices: {voices[0]}, {voices[1]}")
     print(f"start: {datetime.now()}")
     self.pipeline_running = True
 
-    # --- 1. セリフ生成 (CrewAI + OpenAI) ---
+    #": " --- 1. セリフ生成 (CrewAI + OpenAI)": " ---
     pipeline = DialoguePipeline(
         prompt_path="prompt_example.txt",
         # output_dir="output",
         # model=os.getenv("OPENAI_MODEL", "gpt-4o"),
         model=os.getenv("GEMINI_LLM_MODEL", "gpt-4o"),
         temperature=float(os.getenv("TEMPERATURE", "0.8")),
-        per_scene_length={ 1: 1000, 2: 2000, 3: 2000, 4: 1500 }
+        per_scene_length={ 1: 800, 2: 2000, 3: 2000, 4: 1500 }
     )
 
     results = pipeline.run()
@@ -50,20 +108,22 @@ class PipelineManager:
     print(f"\nセリフ生成完了: 全 {len(results)} シーン / 合計 {total} 字")
     print(f"next: {datetime.now()}")
 
-    # --- 2. 音声生成 (Gemini TTS) ---
+    #": " --- 2. 音声生成 (Gemini TTS)": " ---
     dp = [
-      """
-      ### DIRECTOR'S NOTES
+      # """
+      # ### DIRECTOR'S NOTES
 
-      Pacing: Speaks at an energetic pace, keeping up with the extremely fast, rapid
-      """,
+      # Pacing: Speaks at an energetic pace, keeping up with the extremely fast, rapid
+      # """,
       """
       ### DIRECTOR'S NOTES FOR ドローン
 
+      Character: Old woman
       Pacing: Speaks at an energetic pace, keeping up with the extremely fast, rapid
 
       ### DIRECTOR'S NOTES FOR カタパルト
 
+      Character: Young man, high-tone voice
       Pacing: Speaks at an exhausted pace, keeping up with the extremely slow
       """,
       """
@@ -85,7 +145,10 @@ class PipelineManager:
 
     tts = TTSPipeline(
         output_dir=pipeline.output_dir,
-        voices={"ドローン": "Kore", "カタパルト": "Enceladus"},
+        voices={
+          "ドローン": voices[0],
+          "カタパルト": voices[1]
+        },
         model=os.getenv("GEMINI_TTS_MODEL", "gemini-2.5-flash-tts"),
         chunk_max_bytes=5000,
         director_prompt=dp
@@ -97,7 +160,7 @@ class PipelineManager:
     print(f"\n音声生成完了: {len(wav_files)} ファイル")
     print(f"next: {datetime.now()}")
 
-    # --- 3. Forced Alignment (ElevenLabs) ---
+    #": " --- 3. Forced Alignment (ElevenLabs)": " ---
     aligner = AlignmentPipeline(
       output_dir=pipeline.output_dir,
       api_key=os.getenv("ELEVENLABS_API_KEY", None)
@@ -112,15 +175,11 @@ class PipelineManager:
 
     # TODO: error statte
     print(Path(aligned_tsvs[-1]).resolve(), Path(wav_files[-1]).resolve())
-    SimpleUDPClient(self.player_address, self.player_port).send_message(
-      "/load_files", [
-        str(Path(wav_files[-1]).resolve()),
-        str(Path(aligned_tsvs[-1]).resolve())
-      ]
-    )
-    # TODO: error statte
-    SimpleUDPClient(client_address[0], 12001).send_message("/reply", 1)
     self.pipeline_running = False
+    return [
+      str(Path(wav_files[-1]).resolve()),
+      str(Path(aligned_tsvs[-1]).resolve())
+    ]
 
 manager = PipelineManager(
   player_address = os.getenv("PLAYER_OSC_ADDR", "127.0.0.1"),
